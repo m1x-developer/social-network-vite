97931bfe7d56ba5630abe4bb4196dfbd124591b7
Кирилл Ежов <skymole@MacBook-Air-Kirill.local>
2023-02-19 15:50:14 +0400

    Test push

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { RootProvider } from './providers/providers';
 import { AppRoutes } from './routes';
 
+/* testing push in GIT */
+
 export const WrappedApp = () => {
   return (
     <RootProvider>
